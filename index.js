82683416b9775ade0b594b051917680dbaecc9a5
Abel Koech <koechabel161@gmail.com>
2024-06-25 13:15:00 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1=6.2;
+const num2=10;
+const multiply =num1*num2;
+const random = Math.floor(Math.random() * 10)+1;
+const num3=6;
+const num4=16;
+const mod = 16 % 6;
+const max= Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
